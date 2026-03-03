d48f222511c9ada161c82518217a71a3765e5c89
AdityaEXP <adityadevservice@gmail.com>
2026-03-03 11:11:30 +0530

    added tags.json and csv download from gdrive option

--- app.py
+++ app.py
@@ -17,6 +17,18 @@ if not os.path.exists(file_path):
     st.write("Downloading embeddings...")
     gdown.download(url, file_path, quiet=False)
 
+url_tags = "https://drive.google.com/uc?id=1idjtvXhrESMGPUNTaAwMaOsacxMwDhxT"
+file_path_tags = "./data/tags.json"
+if not os.path.exists(file_path_tags):
+    st.write("Downloading tags...")
+    gdown.download(url_tags, file_path_tags, quiet=False)
+
+url_csv = "https://drive.google.com/uc?id=1dWzFITd2bsGOvFYoA0ltbNvZCtzpa_kd"
+file_path_csv = "./data/core_dataset_with_index.csv"
+if not os.path.exists(file_path_csv):
+    st.write("Downloading dataset...")
+    gdown.download(url_csv, file_path_csv, quiet=False)
+
 from scripts.recommend import search
 
 
